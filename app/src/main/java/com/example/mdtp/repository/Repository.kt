db6fecfd1e048155e7e488ed49201578d60fd009
Stas Latyshev <postal915@gmail.com>
2021-12-10 13:24:41 +0200

    Revert "Feature/pagination"

--- app/src/main/java/com/example/mdtp/repository/Repository.kt
+++ app/src/main/java/com/example/mdtp/repository/Repository.kt
@@ -1,15 +1,8 @@
 package com.example.mdtp.repository
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import com.example.mdtp.api.RetrofitInstance
 import com.example.mdtp.model.detail.MovieDetailDTO
 import com.example.mdtp.model.movie.DataDTO
-import com.example.mdtp.model.movie.MovieDTO
-import com.example.mdtp.paging.MoviesPagingSource
-import com.example.mdtp.paging.SearchMoviePagingSource
-import kotlinx.coroutines.flow.Flow
+import com.example.mdtp.api.RetrofitInstance
 import retrofit2.Response
 
 class Repository {
@@ -18,31 +11,7 @@ class Repository {
         return RetrofitInstance.api.getMovies()
     }
 
-    suspend fun getMovieDetail(movieId: Int): Response<MovieDetailDTO> {
+    suspend fun getMovieDetail(movieId: Int):Response<MovieDetailDTO>{
         return RetrofitInstance.api.getMovieDetail(movieId)
-    }
-
-    suspend fun getSearchMovie(searchQuery: String): Response<DataDTO> {
-        return RetrofitInstance.api.getSearchMovie(searchQuery = searchQuery)
-    }
-
-    fun getMoviesPaging(): Pager<Int, MovieDTO> {
-        return Pager(
-            config = PagingConfig(pageSize = 20, enablePlaceholders = false),
-            pagingSourceFactory = { MoviesPagingSource() }
-        )
-    }
-
-    fun getMoviesPagingFlow(): Flow<PagingData<MovieDTO>> {
-        return Pager(
-            config = PagingConfig(pageSize = 20, enablePlaceholders = false),
-            pagingSourceFactory = { MoviesPagingSource() }
-        ).flow
-    }
-
-    fun getSearchMoviePagingFlow(searchQuery: String): Flow<PagingData<MovieDTO>> {
-        return Pager(config = PagingConfig(pageSize = 20, enablePlaceholders = false),
-            pagingSourceFactory = { SearchMoviePagingSource(searchQuery) }
-        ).flow
     }
 }
